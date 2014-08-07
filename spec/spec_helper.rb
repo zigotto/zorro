815a448f51bb93c29569db60af717c13d8d29051
Jesus Lopes <jlopes@zigotto.com.br>
2014-08-06 21:10:17 -0300

    Rspec setup

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+RSpec.configure do |config|
+  Kernel.srand config.seed
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.profile_examples = 10
+  config.order = :random
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
